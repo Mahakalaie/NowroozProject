008e503a82f8637b6d1252fc2a37ba2ddf906718
Mahak <Mahakalaie1@gmail.com>
2025-04-04 00:39:00 +0330

    create getEntityCode method

--- src/example/Human.java
+++ src/example/Human.java
@@ -3,6 +3,9 @@ package example;
 import db.Entity;
 
 public class Human extends Entity {
+
+    public static final int HUMAN_ENTITY_CODE = 14;
+
     public String name;
     public int age;
 
@@ -17,5 +20,10 @@ public class Human extends Entity {
         copyHuman.id = id;
 
         return copyHuman;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return HUMAN_ENTITY_CODE;
     }
 }
